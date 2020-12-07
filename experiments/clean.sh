a405577f0cec93b3710b03bf4fecf215ba31190e
Florian Schroegendorfer <florian.schroegendorfer@phlo.at>
2020-12-07 03:53:11 +0100

    added script to cleanup experiments

---
+++ experiments/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+[ "$1" = "-f" ] && force=yes || force=no
+
+pattern='.*\(boolector\|btormc\|cvc4\)-\(btor2\|functional\|relational\)\..*'
+
+find $(dirname $0)/litmus -regex $pattern | sort
+find $(dirname $0)/count_stat -name buggy.* -o -name cas.* -type d | sort
